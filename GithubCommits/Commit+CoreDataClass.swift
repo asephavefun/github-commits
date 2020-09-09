4c25d693b4277a3ca349ac1f88af167dad4fee50
Hikmat <asephikmatf@gmail.com>
2020-09-09 18:58:11 +0700

    managed to save to core data from api then load it into table view

---
+++ GithubCommits/Commit+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Commit+CoreDataClass.swift
+//  GithubCommits
+//
+//  Created by Fatah on 09/09/20.
+//  Copyright © 2020 AHF. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
